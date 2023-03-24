365ac7fe8f0e87097afeb68cb6e148c3ba45d150
Kent Brekke <kamundsen@live.no>
2023-03-24 20:42:26 +0100

    Adds bundling

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from 'esbuild-wasm';
 import {useState, useEffect, useRef} from 'react';
 import ReactDOM from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 
 const App = () => {
@@ -24,12 +25,14 @@ const App = () => {
             return;
         };
 
-        const result = await ref.current.transform(input, {
-            loader: 'jsx',
-            target: 'es2015'
-        });
-
-        setCode(result.code);
+        const result = await ref.current.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin()]
+        })
+        // console.log(result);
+        setCode(result.outputFiles[0].text);
     };
 
     return (
