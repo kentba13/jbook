717dfc51d96330f82b9748ee411325007f5b80a2
Kent Brekke <kamundsen@live.no>
2023-03-24 19:28:45 +0100

    Adds transpiling

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,49 @@
 import * as esbuild from 'esbuild-wasm';
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef} from 'react';
 import ReactDOM from 'react-dom';
 
 
 const App = () => {
+    const ref = useRef<any>();
     const [input, setInput] = useState('');
     const [code, setCode] = useState('');
 
     const startService = async () => {
-        const service = await esbuild.startService({
+        ref.current = await esbuild.startService({
             worker: true,
             wasmURL: '/esbuild.wasm'
-        });
-        console.log(service);
-    }
+        });      
+    };
 
     useEffect(() => {
         startService();
     }, []);
 
-    const onClick = () => {
-        console.log(input);
-    }
+    const onClick = async () => {
+        if(!ref.current){
+            return;
+        };
 
-    return <div>
-        <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+        const result = await ref.current.transform(input, {
+            loader: 'jsx',
+            target: 'es2015'
+        });
+
+        setCode(result.code);
+    };
+
+    return (
+        <div>
+            <textarea 
+                value={input} 
+                onChange={(e) => setInput(e.target.value)}>
+            </textarea>
             <div>
                 <button onClick={onClick}>Submit</button>
             </div>
-            <pre>{code}</pre>
+            <pre>{code}</pre>     
         </div>
+        );
 }
 
 ReactDOM.render(
