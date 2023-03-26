fc3207ece4e4caea1f975ffb94fd5d82d4a10dd0
Kent Brekke <kamundsen@live.no>
2023-03-26 20:36:11 +0200

    Adds a preview component

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import * as esbuild from 'esbuild-wasm';
 import {useState, useEffect, useRef} from 'react';
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor';
-import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import Preview from './components/preview';
 
 const App = () => {
     const ref = useRef<any>();
-    const iframe = useRef<any>();
+    const [code, setCode] = useState('');
     const [input, setInput] = useState('');
 
     const startService = async () => {
@@ -27,8 +28,6 @@ const App = () => {
             return;
         };
 
-        iframe.current.srcdoc = html;
-
         const result = await ref.current.build({
             entryPoints: ['index.js'],
             bundle: true,
@@ -41,30 +40,10 @@ const App = () => {
                 'process.env.NODE_ENV': '"production"',
                 global: 'window'
             }
-        })
-    
-        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
-    };
+        });
 
-    const html = `
-        <html>
-            <head></head>
-            <body>
-                <div id="root"></div>
-                <script>
-                    window.addEventListener('message', (event) => {
-                        try{
-                            eval(event.data);
-                        } catch(err){
-                            const root = document.querySelector('#root');
-                            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-                            console.error(err);
-                        }
-                    }, false);
-                </script>
-            </body>
-        </html>
-    `;
+        setCode(result.outputFiles[0].text);
+    };
 
     return (
         <div>
@@ -72,14 +51,10 @@ const App = () => {
                 initialValue="const a = 1;" 
                 onChange={(value) => setInput(value)}
                 /> 
-            <textarea 
-                value={input} 
-                onChange={(e) => setInput(e.target.value)}>
-            </textarea>
             <div>
                 <button onClick={onClick}>Submit</button>
             </div>  
-            <iframe title="preview" ref={iframe} srcDoc={html} sandbox="allow-scripts"/> 
+            <Preview code={code}/>
         </div>
         );
 };
