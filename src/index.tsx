6727387a228bf2577d33004fac857eca92815c73
Kent Brekke <kamundsen@live.no>
2023-03-26 20:27:00 +0200

    Adds monaco editor

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import {useState, useEffect, useRef} from 'react';
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
-
+import CodeEditor from './components/code-editor';
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
 const App = () => {
     const ref = useRef<any>();
@@ -67,6 +68,10 @@ const App = () => {
 
     return (
         <div>
+            <CodeEditor 
+                initialValue="const a = 1;" 
+                onChange={(value) => setInput(value)}
+                /> 
             <textarea 
                 value={input} 
                 onChange={(e) => setInput(e.target.value)}>
