472056fb53012679a94bdb8e9e0f49c770b450b8
Kent Brekke <kamundsen@live.no>
2023-03-27 00:40:10 +0200

    Improve error handling

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -3,6 +3,7 @@ import { useRef, useEffect } from 'react';
 
 interface PreviewProps{
     code: string;
+    err: string;
 }
 
 const html = `
@@ -18,13 +19,22 @@ const html = `
             <body>
                 <div id="root"></div>
                 <script>
+                    const handleError = ((err) => {
+                        const root = document.querySelector('#root');
+                        root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+                        console.error(err);
+                    });
+
+                    window.addEventListener('error', (event) => {
+                        event.preventDefault();
+                        handleError(event.error);
+                    });
+
                     window.addEventListener('message', (event) => {
                         try{
                             eval(event.data);
                         } catch(err){
-                            const root = document.querySelector('#root');
-                            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-                            console.error(err);
+                            handleError(err);
                         }
                     }, false);
                 </script>
@@ -32,14 +42,15 @@ const html = `
         </html>
     `;
 
-const Preview: React.FC<PreviewProps> = ({code}) => {
+const Preview: React.FC<PreviewProps> = ({code}, {err}) => {
     const iframe = useRef<any>();
 
     useEffect(() => {
         iframe.current.srcdoc = html;
         setTimeout(() => {
             iframe.current.contentWindow.postMessage(code, '*');
-        }, 50);      
+        }, 50);
+        
     }, [code]);
 
     return (
