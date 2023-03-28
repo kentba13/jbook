2df119f583b593b33e0627778ff2f18eadad63ca
Kent Brekke <kamundsen@live.no>
2023-03-28 21:05:47 +0200

    Adds redux app and handle state of cells

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {store} from './state';
 import TextEditor from './components/text-editor';
 
 const App = () => {
     return (
+        <Provider store={store}>
         <div>
             <TextEditor></TextEditor>
         </div>
+        </Provider>
         );
 };
 
